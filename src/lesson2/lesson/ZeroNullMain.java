d6c57ae0f89353ffff534d99776bb5acbc100ef3
tit <titovsky@list.ru>
2022-02-21 11:19:17 +0300

    lesson

---
+++ src/lesson2/lesson/ZeroNullMain.java
@@ -0,0 +1,48 @@
+package lesson2.lesson;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ZeroNullMain {
+
+    public static void main(String[] args) throws IOException {
+
+        Integer result = 0;
+        Integer a = 100;
+        Integer b = 1000;
+
+        try{
+            result = b/a;
+        } catch (NullPointerException | ArithmeticException ae){
+            ae.printStackTrace();
+            for(StackTraceElement stack : ae.getStackTrace()){
+                System.out.println(stack.toString());
+            }
+            result = 100;
+        }
+
+        System.out.println(result);
+
+        BufferedReader br = null;
+        try {
+
+            br = new BufferedReader(new FileReader("12312323"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+        }
+
+        try(BufferedReader br1 = new BufferedReader(new FileReader("12312323"))){
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+}
